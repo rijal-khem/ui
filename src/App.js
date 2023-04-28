c517eea3ad889d7eb7c42b34bb4a1bffdb2707bd
rizal <rijalkhem187@gmail.com>
2023-04-28 00:48:33 -0400

    Added About page.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import LogIn from './components/Login'
 import Card from './components/Card';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       
-    <Card/>
+    <About/>
 
     
     </div>
