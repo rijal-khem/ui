ad16917b29c85e63365957341982c725621b8165
rizal <rijalkhem187@gmail.com>
2023-04-29 00:20:05 -0400

    Removed unnecessary stuffs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import About from './components/About';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
-    <About/>
-
-    
+    <Navbar/>
+    <Main/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+
+function Navbar () {
+
+    return (
+        <div className='flex'>
+           <ul>
+            <li>About</li>
+            <li>Visualization</li>
+            <li>Games</li>
+            <li>Rizzkitch</li>
+           </ul>
+        </div>
+    )
+}
+
+export default Navbar;
