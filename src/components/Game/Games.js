c5f9dd0525b9b5971b7aa8d641ef1b7bdca2e2d9
rizal <rijalkhem187@gmail.com>
2023-05-22 20:23:32 -0400

    Corrected Button working.

--- src/components/Game/Games.js
+++ src/components/Game/Games.js
@@ -6,24 +6,23 @@ import Form from './Form'
 function Games () {
 
     const [game, setGame] = useState(null);
-    const [playerName, setPlayerName] = useState(null)
-
-
-    
-    
-     
+    const [playerName, setPlayerName] = useState(null);
     return (
     <div className="game-container">
 
-    <div className='game-view'>
+        <div className="">
 
-    {game===null&&playerName===null && <Form  setPlayerName={setPlayerName} setGame={setGame}/>}
-        
-    {game==="FalashGame" && <FalashGame playerName={playerName}/>}
-    
+                <div className='game-view'>
 
-    </div>
+                    {game===null&&playerName===null && <Form  setPlayerName={setPlayerName} setGame={setGame}/>}
+                        
+                    {game==="FalashGame" && <FalashGame playerName={playerName}/>}
+                </div>
 
+
+        </div>
+
+            
     </div>)
 }
 
